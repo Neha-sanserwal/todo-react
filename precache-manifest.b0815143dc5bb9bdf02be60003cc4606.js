3e4874a0625181919a97b92d3da084c05fdac94e
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-08-29 17:32:37 +0530

    Updates

---
+++ precache-manifest.b0815143dc5bb9bdf02be60003cc4606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4377bd68a98dc234aba1a50b63daca8",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "dfd58dbd13bae5472fe0",
+    "url": "/todo-react/static/css/main.3bb997e8.chunk.css"
+  },
+  {
+    "revision": "0e391ffc5477401acd19",
+    "url": "/todo-react/static/js/2.93e4f528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.93e4f528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd58dbd13bae5472fe0",
+    "url": "/todo-react/static/js/main.77ee9528.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);

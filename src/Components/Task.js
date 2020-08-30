257c4442b1d271a99d8b0ab68227878723bffca8
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-08-30 13:05:40 +0530

    resolved error of this.props

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,13 +1,10 @@
 import React from "react";
 
 const Task = (props) => {
-  const { isCompleted, message, taskId } = props;
+  const { isCompleted, message, taskId, handleClick } = props;
   const className = isCompleted ? "complete" : "un-complete";
   return (
-    <div
-      className={`task  ${className}`}
-      onClick={() => this.props.handleClick(taskId)}
-    >
+    <div className={`task  ${className}`} onClick={() => handleClick(taskId)}>
       <div className="indicator"></div>
       <div className="message" key={taskId}>
         {message}

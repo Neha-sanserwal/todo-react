c07fe375bea5a10718551af00137b39871555057
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-09-02 06:45:51 +0530

    added a delete button for task

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 const Task = (props) => {
-  const { status, message, taskId, handleClick } = props;
+  const { status, message, taskId, handleClick, handleDelete } = props;
   return (
-    <div className={`task  ${status}`} onClick={() => handleClick(taskId)}>
-      <div className="indicator"></div>
-      <div className="message" key={taskId}>
-        {message}
+    <div>
+      <div className={`task  ${status}`} onClick={() => handleClick(taskId)}>
+        <div className="indicator"></div>
+        <div className="message" key={taskId}>
+          {message}
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          alert("clicked");
+        }}
+      >
+        X
       </div>
     </div>
   );

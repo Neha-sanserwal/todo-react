3ebafe4cadd9f0855bcb6cdf7761a626f33a6f41
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-09-02 12:45:06 +0530

    added react hover event

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Task = (props) => {
-  const { status, message, taskId, toggleStatus, deleteTask } = props;
-  return (
-    <div className="task-box">
-      <div className={`task ${status}`} onClick={() => toggleStatus(taskId)}>
-        <div className="indicator"></div>
-        <div className="message" key={taskId}>
-          {message}
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovering: false,
+    };
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+  }
+
+  handleMouseHover() {
+    this.setState((prevState) => ({ isHovering: !prevState.isHovering }));
+  }
+
+  render() {
+    const { status, message, taskId, toggleStatus, deleteTask } = this.props;
+    return (
+      <div
+        className="task-box"
+        onMouseEnter={this.handleMouseHover}
+        onMouseLeave={this.handleMouseHover}
+      >
+        <div className={`task ${status}`} onClick={() => toggleStatus(taskId)}>
+          <div className="indicator"></div>
+          <div className="message" key={taskId}>
+            {message}
+          </div>
         </div>
+        {this.state.isHovering && (
+          <div className="delete-btn" onClick={() => deleteTask(taskId)}>
+            X
+          </div>
+        )}
       </div>
-      <div className="delete-btn" onClick={() => deleteTask(taskId)}>
-        X
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Task;

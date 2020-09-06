5baf9a6051bdc22aba819b9158656cab2af060ba
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-09-06 18:43:56 +0530

    removed the the create and filter task method

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -11,25 +11,17 @@ const Todo = (props) => {
   const [tasks, setTasks] = useState([]);
   const [lastTodoId, setLastTodoId] = useState(0);
 
-  const createNewTask = (message, id) => {
-    return {
-      message,
-      status: getDefaultStatus(),
-      taskId: id,
-    };
-  }; // gives a task object
-
   const saveTask = (message) => {
     setTasks((tasks) => {
-      const task = createNewTask(message, lastTodoId);
-      return [...tasks, task];
+      const status = getDefaultStatus();
+      return [...tasks, { message, status, taskId: lastTodoId }];
     });
     setLastTodoId((lastTodoId) => lastTodoId + 1);
   }; // saves task in state
 
   const toggleTaskStatus = (taskId) => {
     setTasks((tasks) => {
-      const tasksCopy = tasks.map((task) => ({ ...task })); // Deep cloning the tasks
+      const tasksCopy = tasks.map((task) => ({ ...task })); // clone
       const taskToUpdate = tasksCopy.find((task) => task.taskId === taskId); // finding task to toggle
       taskToUpdate.status = getNextStatus(taskToUpdate.status); // get next status
       return tasksCopy; // update the state
@@ -46,18 +38,11 @@ const Todo = (props) => {
     setHeading(props.heading);
   };
 
-  const filterTasks = (tasks, taskId) => {
-    const tasksCopy = tasks.map((task) => ({ ...task })); // Deep cloning
-    const updatedTasks = tasksCopy.filter((task) => task.taskId !== taskId); // filtering tasks to not delete
-    return updatedTasks; // updating tasks
-  };
-
   const deleteTask = (taskId) => {
-    setTasks((prevTasks) => filterTasks(prevTasks, taskId));
+    setTasks((prevTasks) => prevTasks.filter((task) => task.taskId !== taskId));
   };
 
   const HeadingWithDelete = WithDelete(TasksHeading, deleteTasks);
-
   return (
     <div className="todo">
       <HeadingWithDelete value={heading} changeHeading={changeHeading} />

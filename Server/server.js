8df74b7a22e1d82bd1a0a40e458d4022d09aca06
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-09-08 18:22:38 +0530

    updating heading

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,6 @@ let todo = { heading: "Todo new", tasks: [], lastTodoId: 0 };
 app.use(express.json());
 
 app.get("/api/getCurrentHeading", (req, res) => {
-  console.log({ heading: todo.heading });
   res.json({ heading: todo.heading });
 });
 
@@ -14,8 +13,14 @@ app.get("/api/getLastTodoId", (req, res) => {
   res.json({ lastTodoId: todo.lastTodoId });
 });
 
+app.post("/api/updateHeading", (req, res) => {
+  const { heading } = req.body;
+  todo.heading = heading;
+  res.end();
+});
+
 app.get("/api/getAllTasks", (req, res) => {
-  res.json({ lastTodoId: todo.tasks });
+  res.json({ tasks: todo.tasks });
 });
 
 app.listen(port, () => console.log(`Example app listening on port port!`));
--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,16 @@
-export const updateHeading = () => {
+const postReq = (url, data) => {
+  return fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+};
+
+export const updateHeading = (value) => {
   return new Promise((resolve, reject) => {
-    resolve(true);
+    postReq("/api/updateHeading", { heading: value })
+      .then((res) => res.json)
+      .then(() => resolve(true));
   });
 };
 export const getCurrentHeading = () => {
@@ -12,7 +22,7 @@ export const getCurrentHeading = () => {
 };
 export const getAllTasks = () => {
   return new Promise((resolve, reject) => {
-    fetch("/api//api/getAllTasks")
+    fetch("/api/getAllTasks")
       .then((res) => res.json())
       .then(resolve);
   });

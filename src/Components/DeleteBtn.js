9e7ad0cd2006cf2b0865375e031e95d662974dc4
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-02 13:33:34 +0530

    added style component on task

--- src/Components/DeleteBtn.js
+++ src/Components/DeleteBtn.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+const DltBtn = styled.div`
+  display: flex;
+  cursor: pointer;
+  font-weight: 800;
+  color: gray;
+  align-items: center;
+  font-size: 1.2rem;
+`;
 export default (props) => {
   const { id, onClick } = props;
-  const DltBtn = styled.div`
-    display: flex;
-    cursor: pointer;
-    font-weight: 800;
-    color: gray;
-    align-items: center;
-    font-size: 1.2rem;
-  `;
+
   return <DltBtn onClick={() => onClick(id)}>X</DltBtn>;
 };
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+const COLORS = {
+  due: "lightblue",
+  processing: "orange",
+  completed: "seagreen",
+};
+const Indicator = styled.span`
+  height: 30px;
+  margin-left: 1rem;
+  width: 20px;
+  margin-right: 1rem;
+  background: ${(props) => COLORS[props.color]};
+`;
+
 const Task = (props) => {
   const { status, message, id, toggleStatus } = props;
   return (
     <div className={`task ${status}`} onClick={toggleStatus}>
-      <div className="indicator"></div>
+      <Indicator color={status}></Indicator>
       <div className="message" key={id}>
         {message}
       </div>
